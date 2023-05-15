bfbf543e40a859c40503d1dc44dcac6cf748bf2b
BatistaTony <batistatomeoliveira96@gmail.com>
2023-05-15 10:40:51 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Batista Tone on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
